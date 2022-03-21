38f6443a98e9341a8f86ffa14fd4cbf0eebed2c8
Koppishetty <2088577@cognizant.com>
2022-03-21 15:35:06 +0530

    MenuItem

---
+++ MenuItemListing/Controllers/MenuItemController.cs
@@ -0,0 +1,74 @@
+﻿using MenuItemListing.model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenuItemListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+       [HttpGet]
+       public List<MenuItem> Get()
+        {
+            List<MenuItem> menuItems = new List<MenuItem>();
+            new MenuItem() { Id = 1, Name = "Pizza", Active = true, DateOFLaunch = new DateTime(2019, 01, 02), FreeDelivery = false, Price = 300 };
+            new MenuItem()
+            {
+                Id = 2,
+                Name = "Burger",
+                Active = true,
+                DateOFLaunch = new DateTime(2018, 03, 12),
+                FreeDelivery = false,
+                Price = 500
+            };
+            new MenuItem()
+            {
+                Id = 3,
+                Name = "Biryani",
+                Active = true,
+                DateOFLaunch = new DateTime(2014, 12, 11),
+                FreeDelivery = true
+                ,
+                Price = 300
+            };
+            return menuItems;
+
+            }
+
+        [HttpGet("{id}")]
+        public MenuItem GetMenuItemById(int id)
+        {
+            List<MenuItem> menuItems = new List<MenuItem>();
+            new MenuItem() { Id = 1, Name = "Pizza", Active = true, DateOFLaunch = new DateTime(2019, 01, 02), FreeDelivery = false, Price = 300 };
+            new MenuItem()
+            {
+                Id = 2,
+                Name = "Burger",
+                Active = true,
+                DateOFLaunch = new DateTime(2018, 03, 12),
+                FreeDelivery = false,
+                Price = 500
+            };
+            new MenuItem()
+            {
+                Id = 3,
+                Name = "Biryani",
+                Active = true,
+                DateOFLaunch = new DateTime(2014, 12, 11),
+                FreeDelivery = true
+                ,
+                Price = 300
+            };
+            MenuItem obj = menuItems.SingleOrDefault(i => i.Id == id);
+            return obj;
+
+        }
+
+        }
+    }
+
---
+++ MenuItemListing/model/MenuItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenuItemListing.model
+{
+    public class MenuItem
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Display(Name="Free Delivery")]
+       
+        public Boolean FreeDelivery { get; set; }
+        public Double Price { get; set; }
+      
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd")]
+       
+         public DateTime DateOFLaunch { get; set; }
+        public Boolean Active { get; set; }
+    }
+}
